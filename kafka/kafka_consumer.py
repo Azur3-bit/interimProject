344e354a9e40f65328d8680dea0011427927ec8a
Sunil <73417492+Azur3-bit@users.noreply.github.com>
2025-02-13 23:49:39 +0530

    Update kafka_consumer.py

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -1,7 +1,10 @@
 from kafka import KafkaConsumer
 
 # Initialize Kafka Consumer
-consumer = KafkaConsumer('log_topic', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
+# consumer = KafkaConsumer('log_topic', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
+
+consumer = KafkaConsumer('server-logs', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
+
 
 print("Consuming log messages from Kafka topic 'log_topic':")
 for message in consumer:
