5fc3f741b83b5cab428649c7ae8387af9a908656
Sunil <sharma.suni12527@gmail.com>
2025-02-14 00:03:20 +0530

    changed kafka_consumer

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -1,8 +1,8 @@
 from kafka import KafkaConsumer
 
 # Initialize Kafka Consumer
-consumer = KafkaConsumer('log_topic', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
+consumer = KafkaConsumer('server_logs', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
 
-print("Consuming log messages from Kafka topic 'log_topic':")
+print("Consuming log messages from Kafka topic 'server_logs':")
 for message in consumer:
     print(message.value.decode('utf-8'))
