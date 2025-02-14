cbb3667ff25398b3009755d3efbea20a0ee9ca9b
Sunil <73417492+Azur3-bit@users.noreply.github.com>
2025-02-14 09:32:20 +0530

    Update kafka_consumer.py

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -4,9 +4,9 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer(
     'server-logs',  # Topic name (ensure it matches producer topic)
     bootstrap_servers='localhost:9092',
-    auto_offset_reset='earliest',  # Read messages from the beginning
-    enable_auto_commit=True,  # Automatically commit offsets
-    group_id='log-consumer-group',  # Consumer group to track offsets
+    auto_offset_reset='earliest',  # Read messages from the beginning 
+    enable_auto_commit=True,  # Automatically commit offsets  (can be ignored default) 
+    group_id='log-consumer-group',  # Consumer group to track offsets -- (remove this and check optional command) 
 )
 
 print("Consuming log messages from Kafka topic 'server-logs':")
