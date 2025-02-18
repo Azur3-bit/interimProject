44a3c1ff01c15dd6b95deecca87caddf06d986d3
Sunil <73417492+Azur3-bit@users.noreply.github.com>
2025-02-18 10:24:56 +0530

    Update kafka_producer.py

--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -3,10 +3,8 @@ import random
 import time
 from datetime import datetime
 
-# Initialize Kafka Producer
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-# Sample log levels
 log_levels = ['INFO', 'WARNING', 'ERROR']
 
 while True:
@@ -14,7 +12,6 @@ while True:
     log_level = random.choice(log_levels)
     message = f'{timestamp} - {log_level} - Sample log message'
     
-    # Send message to Kafka topic
     producer.send('server-logs', message.encode('utf-8'))
     print(f'Produced: {message}')
     
